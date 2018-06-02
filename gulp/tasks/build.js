af1909aa9e430a039391179ffae32108ebadf197
Razvan <obioanrazvan@gmail.com>
2018-06-02 20:47:31 +0300

    am creat dist folderul dar am ceva comportament ciudat cu mobile menu

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,9 +1,43 @@
 var gulp = require("gulp"),
-imagemin = require('gulp-imagemin');
+imagemin = require('gulp-imagemin'),
+del = require('del'),
+rev = require("gulp-rev"),
+cssnano = require("gulp-cssnano"),
+uglify = require("gulp-uglify"),
+usemin = require('gulp-usemin'),
+browserSync = require('browser-sync').create();
 
-gulp.task("build",function(){
-	
+gulp.task('previewDist', function() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "dist"
+    }
+  });
+ });
+
+
+gulp.task('deleteDistFolder', function() {
+  return del("./dist");
+});
+
+gulp.task("copyGeneralFiles",["deleteDistFolder"], function(){
+  var pathsToCopy = [
+  "./app/**/*",
+   "!./app/index.html",
+   "!./app/assets/images/**",
+   "!./app/assets/styles/**",
+   "!./app/assets/scripts/**",
+   "!./app/temp",
+   "!./app/temp/**"
+
+
+   ]
+  return gulp.src(pathsToCopy)
+  .pipe(gulp.dest("./dist"));
 })
+
+
 gulp.task('optimizeImages', ['deleteDistFolder'], function() {
   return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
     .pipe(imagemin({
@@ -13,3 +47,17 @@ gulp.task('optimizeImages', ['deleteDistFolder'], function() {
     }))
     .pipe(gulp.dest("./dist/assets/images"));
 });
+
+gulp.task('usemin', ['deleteDistFolder'], function() {
+  return gulp.src("./app/index.html")
+    .pipe(usemin({
+      css:[function(){return rev()}, function(){ return cssnano()}],
+      js:[function(){return rev()}, function(){return uglify()}]
+    }))
+    .pipe(gulp.dest("./dist"));
+});
+
+
+
+
+gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages',"usemin"]);
